4b577b2105966e827cb048863cfdbe40bbeddd57
user <user@LAPTOP-7RSHCGQ9>
2026-06-17 20:02:34 +0530

    first commit

---
+++ DemoStudent/src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Student;
+
+public interface StudentService {
+	
+	void add(Student S);
+	List<Student> display();
+	
+	
+}
---
+++ DemoStudent/src/main/java/com/example/demo/service/StudentServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Student;
+
+public class StudentServiceImpl implements StudentService {
+
+	@Override
+	public void add(Student S) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Student> display() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
